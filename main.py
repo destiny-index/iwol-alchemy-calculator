61fdb602125dc48dee67f025122e17b682678d80
Anthony Poon <marquis.andras@gmail.com>
2022-05-03 23:42:07 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,207 @@
+import pandas as pd
+import sys
+from functools import reduce, cache
+
+spreadsheet = 'IWOL Alchemy and Forging Guide.xlsx'
+
+@cache
+def get_recipes():
+    names = ['grade', None, 'recipe_name', 'slot', 'quantity', 'herb', None, None, 'potency', 'property']
+    data = pd.read_excel(spreadsheet, sheet_name='Recipes', skiprows=2, header=None, usecols='B:K', names=names)
+
+    # Assemble the recipe from the rows that describe each ingredient slot
+    recipes = {}
+    for ingredient in data.itertuples():
+        if ingredient.recipe_name not in recipes:
+            recipes[ingredient.recipe_name] = {}
+
+        if isinstance(ingredient.herb, str):
+            recipes[ingredient.recipe_name][ingredient.slot] = {
+                'quantity': ingredient.quantity,
+                'herb': get_herb(name=ingredient.herb)
+            }
+
+    return recipes
+
+
+@cache
+def get_herbs():
+    data = pd.read_excel(spreadsheet, sheet_name='Herbs', usecols='A:C,E,G')
+    return list(data.itertuples())
+
+
+def get_herb(name):
+    return next((h for h in get_herbs() if h.Name == name), None)
+
+
+def count_num_herbs(recipe):
+    return reduce(lambda total, slot: total + slot['quantity'], recipe.values(), 0)
+
+
+def herbs_by(grade=None, property=None):
+    return [
+        h for h in get_herbs()
+        if h.Grade == grade and (
+            h.Primary == property or
+            h.Secondary == property or
+            h.Temperature == property
+        )
+    ]
+
+
+def get_balancing_temperature(recipe):
+    temperatures = [
+        ingredient['herb'].Temperature
+        for slot, ingredient in recipe.items() if slot != 'Temperature'
+    ]
+    if temperatures.count('Cold') > temperatures.count('Heat'):
+        return 'Heat'
+    elif temperatures.count('Cold') < temperatures.count('Heat'):
+        return 'Cold'
+    else:
+        return 'Balanced'
+
+
+def sidetier_ingredient(slot, recipe):
+    herb = recipe[slot]['herb']
+    qty = recipe[slot]['quantity']
+
+    # Drop the number from the slot name when looking up herb property
+    property = getattr(herb, slot.split(' ')[0])
+
+    sidetiered_recipe = [
+        { **recipe, slot: { 'herb': new_herb, 'quantity': qty } }
+        for new_herb in herbs_by(grade=herb.Grade, property=property)
+        if new_herb != herb
+    ]
+
+    if slot == 'Temperature': # No temperatures have changed
+        return sidetiered_recipe
+
+    # Figure out the correct temperature herbs for the sidetiered recipes
+    t_herb = recipe['Temperature']['herb']
+    t_qty = recipe['Temperature']['quantity']
+    return [
+        { **r, 'Temperature': { 'herb': h, 'quantity': t_qty } }
+        for r in sidetiered_recipe
+        for h in herbs_by(grade=t_herb.Grade, property=get_balancing_temperature(r))
+    ]
+
+def sidetier(recipe):
+    return [
+        new_recipe
+        for slot in recipe.keys()
+        for new_recipe in sidetier_ingredient(slot, recipe)
+    ]
+
+
+def downtier_ingredient(slot, recipe):
+    herb = recipe[slot]['herb']
+    qty = recipe[slot]['quantity']
+
+    # Drop the number from the slot name when looking up herb property
+    # e.g. 'Primary 2' becomes 'Primary'
+    property = getattr(herb, slot.split(' ')[0])
+
+    # T6 herbs can be replaced by 6 T5
+    # T5 herbs can be replaced by 5 T4 etc.
+    qty_ratio = { 6: 6, 5: 5, 4: 4, 3: 3, 2: 3 }
+
+    downtiered_recipe = [
+        { **recipe, slot: { 'herb': new_herb, 'quantity': qty * qty_ratio[herb.Grade] } }
+        for new_herb in herbs_by(grade=herb.Grade-1, property=property)
+    ]
+
+    if slot == 'Temperature': # No temperatures have changed
+        return downtiered_recipe
+
+    # Figure out the correct temperature herbs for the downtiered recipes
+    t_herb = recipe['Temperature']['herb']
+    t_qty = recipe['Temperature']['quantity']
+    return [
+        { **r, 'Temperature': { 'herb': h, 'quantity': t_qty } }
+        for r in downtiered_recipe
+        for h in herbs_by(grade=t_herb.Grade, property=get_balancing_temperature(r))
+    ]
+
+
+def downtier(recipe, furnace_capacity=14):
+    downtiered_recipes = [
+        new_recipe
+        for slot in recipe.keys()
+        for new_recipe in downtier_ingredient(slot, recipe)
+        if count_num_herbs(new_recipe) <= furnace_capacity
+    ]
+    if len(downtiered_recipes) > 0:
+        return downtiered_recipes + [ j for i in downtiered_recipes for j in downtier(i) ]
+    else:
+        return downtiered_recipes
+
+
+def print_recipe(recipe):
+    print(
+        'Price: {}\t'.format(calculate_price(recipe)),
+        '\t'.join([
+            '{}: {:0.0f}x {} '.format(slot, ingredient['quantity'], ingredient['herb'].Name)
+            for slot, ingredient in recipe.items()
+        ])
+    )
+
+
+def calculate_price(recipe):
+    pricing = { 1: 3, 2: 12, 3: 135, 4: 1440, 5: 13500, 6: 81000 }
+    return sum([
+        pricing[ingredient['herb'].Grade] * ingredient['quantity'] * 2
+        for ingredient in recipe.values()
+    ])
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+
+    recipes = get_recipes()
+
+    print_recipe(recipes[name])
+
+    for i in sidetier(recipes[name]):
+        for j in downtier(i):
+            print_recipe(j)
+
+
+from unittest import TestCase
+class TestRecipes(TestCase):
+    def test_that_herbs_can_be_loaded_from_spreadsheet(self):
+        herbs = get_herbs()
+        self.assertEqual('Azuresky Flower', herbs[0].Name)
+        self.assertEqual('Mending', herbs[0].Primary)
+        self.assertEqual('Focusing', herbs[0].Secondary)
+        self.assertEqual('Heat', herbs[0].Temperature)
+        self.assertEqual(1, herbs[0].Grade)
+
+    def test_that_recipes_can_be_loaded_from_spreadsheet(self):
+        recipes = get_recipes()
+        self.assertEqual({ 'herb': get_herb(name='Waterglade Lily'), 'quantity': 3.0 },
+            recipes['Bright Heart Elixir']['Primary'])
+        self.assertEqual({ 'herb': get_herb(name='Sepastra Herb'), 'quantity': 4.0 },
+            recipes['Bright Heart Elixir']['Secondary'])
+        self.assertEqual({ 'herb': get_herb(name='Panacea Dew'), 'quantity': 2.0 },
+            recipes['Bright Heart Elixir']['Secondary 2'])
+        self.assertEqual({ 'herb': get_herb(name='Panacea Dew'), 'quantity': 1.0 },
+            recipes['Bright Heart Elixir']['Temperature'])
+
+    def test_that_herb_can_be_found_by_grade_and_property(self):
+        found = herbs_by(grade=4.0, property='Cold')
+        self.assertEqual(13, len(found))
+
+    def test_that_the_temperature_of_the_temperature_herb_can_be_calculated(self):
+        recipes = get_recipes()
+        self.assertEqual('Heat', get_balancing_temperature(recipes['Expert Healing Elixir']))
+        self.assertEqual('Cold', get_balancing_temperature(recipes['Divine Heart Elixir']))
+        self.assertEqual('Balanced', get_balancing_temperature(recipes['Azure Heart Elixir']))
+
+    def test_that_recipes_can_be_downtiered(self):
+        recipes = get_recipes()
+        self.assertTrue(len(downtier(recipes['Greater Healing Elixir'])) > 10)
+
+    def test_that_recipes_can_be_sidetiered(self):
+        recipes = get_recipes()
+        self.assertTrue(len(sidetier(recipes['Greater Healing Elixir'])) > 10)
