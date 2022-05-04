95fea1183a9677ae28c6abf2a85a83d0f23aeded
Anthony Poon <marquis.andras@gmail.com>
2022-05-04 14:48:56 +1000

    Revert "Print temperature instead of herb"
    This reverts commit faf06188bf81b101c2eb3978ba81c399636b8dca.

--- main.py
+++ main.py
@@ -182,10 +182,7 @@ def print_recipe(recipe):
     print(
         'Price: {}\t'.format(calculate_price(recipe)),
         '\t'.join([
-            '{}: {:0.0f}x {} '.format(
-                slot,
-                ingredient['quantity'],
-                ingredient['herb'].Name if slot != 'Temperature' else "T{:0.0f} {}".format(ingredient['herb'].Grade, ingredient['herb'].Temperature))
+            '{}: {:0.0f}x {} '.format(slot, ingredient['quantity'], ingredient['herb'].Name)
             for slot, ingredient in recipe.items()
         ])
     )
