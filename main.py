38e71d0936d9646d775a76b43fbe30ebaa2b4d0d
Anthony Poon <marquis.andras@gmail.com>
2022-05-05 14:54:33 +1000

    Fix ordering of printed json fields

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def balance_recipe_temperature(recipe):
 
 def get_recipe_slots(recipe):
     possible_slots = [ 'Primary', 'Primary 2', 'Secondary', 'Secondary 2', 'Temperature' ]
-    return [ slot for slot in recipe.keys() if slot in possible_slots ]
+    return [ slot for slot in possible_slots if slot in recipe.keys()]
 
 
 def sidetier(recipe, furnace_capacity=14, found=[]):
